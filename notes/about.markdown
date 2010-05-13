ff4d1076f2379435c781e06d58d77c2f93912689
Nathan Hamblen <nathan@technically.us>
2010-05-12 23:40:41 -0400

    release prep

---
+++ notes/about.markdown
@@ -0,0 +1,5 @@
+[sxr-publish][sxr-publish] is a plugin for [Simple Build Tool][sbt] to publish source files annotated by [Scala X-Ray][sxr].
+
+[sxr-publish]: http://github.com/n8han/sxr-publish
+[sbt]: http://code.google.com/p/simple-build-tool/
+[sxr]: http://github.com/harrah/browse
--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,9 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends PluginProject(info) {
+class Project(info: ProjectInfo) extends PluginProject(info) with posterous.Publish {
   val dispatch = "net.databinder" %% "dispatch-http" % "0.7.3"
+
+  override def extraTags = "sbt plugin" :: Nil
 
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
